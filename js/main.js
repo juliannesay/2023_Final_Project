5ab3f46853a345efd760b2d3aea9f48bd668790e
juliannesay <jmsay@wisc.edu>
2023-08-28 00:32:23 -0400

    basemap API error fix

--- js/main.js
+++ js/main.js
@@ -2,13 +2,10 @@
 const map = L.map('map').setView([42.7, -75.5], 6);
 L.tileLayer('https://www.stadiamaps.com/').addTo(map);
 
-// Add OSM Bright base tilelayer
-var Stadia_OSMBright = L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.{ext}', {
-	minZoom: 0,
-	maxZoom: 20,
-	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	ext: 'png'
-  }).addTo(map);
+var OSMStandard = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 
   // Add Esri World Imagery basemap
   var Esri_WorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
@@ -17,7 +14,7 @@ var Stadia_OSMBright = L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_brigh
 
   // Create a basemap control layer
   var basemaps = {
-    "Open Street Map Bright": Stadia_OSMBright,
+    "Open Street Map": OSMStandard,
     "Esri World Imagery": Esri_WorldImagery
   };
 
